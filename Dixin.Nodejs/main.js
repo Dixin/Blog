d5ed2a4383084ee518f1ab368c7ed78a4a4a8218
Dixin Yan <dixinyan@live.com>
2016-02-06 08:17:27 -0800

    Update comments.

--- Dixin.Nodejs/main.js
+++ Dixin.Nodejs/main.js
@@ -5,7 +5,7 @@ var tumblr = require("./tumblr");
 tumblr.downloadAllAndUnlike({
     userEmail: "userEmail",
     userPassword: "userPassword",
-    // register an application in tumblr: https://www.tumblr.com/oauth/apps.
+    // Register an application in tumblr: https://www.tumblr.com/oauth/apps.
     appConsumerKey: "appConsumerKey",
     appSecretKey: "appSecretKey",
     offset: 5,
