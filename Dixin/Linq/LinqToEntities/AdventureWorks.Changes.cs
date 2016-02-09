57fe435c9e560163272b1bbbe4b1974d46f0ce95
Dixin Yan <dixinyan@live.com>
2016-02-09 01:32:05 -0800

    Update code analysis rules.

--- Dixin/Linq/LinqToEntities/AdventureWorks.Changes.cs
+++ Dixin/Linq/LinqToEntities/AdventureWorks.Changes.cs
@@ -8,6 +8,7 @@
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Transactions;
 
