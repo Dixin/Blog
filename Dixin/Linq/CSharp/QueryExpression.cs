58e2431279a09f7aeea5a9b7de86931432edd783
Dixin Yan <dixinyan@live.com>
2016-09-24 01:05:52 -0700

    Update examples.

--- Dixin/Linq/CSharp/QueryExpression.cs
+++ Dixin/Linq/CSharp/QueryExpression.cs
@@ -57,61 +57,50 @@
     {
         internal static void LocalQuery(ILocal<Uri> uris)
         {
-            ILocal<ILocalGroup<string, Uri>> query =
+            ILocal<string> query =
                 from uri in uris
                 where uri.IsAbsoluteUri // ILocal.Where and anonymous method.
                 group uri by uri.Host into hostUris // ILocal.GroupBy and anonymous method.
-                let host = hostUris.Key // ILocal.Select and anonymous method.
-                orderby host // ILocal.OrderBy and anonymous method.
-                select hostUris; // ILocal.Select and anonymous method.
+                orderby hostUris.Key // ILocal.OrderBy and anonymous method.
+                select hostUris.ToString(); // ILocal.Select and anonymous method.
         }
 
         internal static void RemoteQuery(IRemote<Uri> uris)
         {
-            IRemote<IRemoteGroup<string, Uri>> query =
+            IRemote<string> query =
                 from uri in uris
                 where uri.IsAbsoluteUri // IRemote.Where and expression tree.
                 group uri by uri.Host into hostUris // IRemote.GroupBy and expression tree.
-                let host = hostUris.Key // IRemote.Select and expression tree.
-                orderby host // IRemote.OrderBy and expression tree.
-                select hostUris; // IRemote.Select and expression tree.
+                orderby hostUris.Key // IRemote.OrderBy and expression tree.
+                select hostUris.ToString(); // IRemote.Select and expression tree.
         }
-    }
 
-    internal static partial class QueryExpression
-    {
         internal static void CompiledLocalQuery(ILocal<Uri> uris)
         {
-            ILocal<ILocalGroup<string, Uri>> query = uris
+            ILocal<string> query = uris
                 .Where(uri => uri.IsAbsoluteUri) // ILocal.Where and anonymous method.
                 .GroupBy(uri => uri.Host) // ILocal.GroupBy and anonymous method.
-                .Select(hostUris => new { hostUris, host = hostUris.Key }) // ILocal.Select and anonymous method.
-                .OrderBy(hostUris => hostUris.host) // ILocal.OrderBy and anonymous method.
-                .Select(hostUris => hostUris.hostUris); // ILocal.Select and anonymous method.
+                .OrderBy(hostUris => hostUris.Key) // ILocal.OrderBy and anonymous method.
+                .Select(hostUris => hostUris.ToString()); // ILocal.Select and anonymous method.
         }
 
         internal static void CompiledRemoteQuery(IRemote<Uri> uris)
         {
-            IRemote<IRemoteGroup<string, Uri>> query = uris
+            IRemote<string> query = uris
                 .Where(uri => uri.IsAbsoluteUri) // IRemote.Where and expression tree.
                 .GroupBy(uri => uri.Host) // IRemote.GroupBy and expression tree.
-                .Select(hostUris => new { hostUris, host = hostUris.Key }) // IRemote.Select and expression tree.
-                .OrderBy(hostUris => hostUris.host) // IRemote.OrderBy and expression tree.
-                .Select(hostUris => hostUris.hostUris); // IRemote.Select and expression tree.
+                .OrderBy(hostUris => hostUris.Key) // IRemote.OrderBy and expression tree.
+                .Select(hostUris => hostUris.ToString()); // IRemote.Select and expression tree.
         }
-    }
 
-    internal static partial class QueryExpression
-    {
         internal static void LinqToObjects(IEnumerable<Type> types)
         {
             IEnumerable<IGrouping<string, Type>> query =
                 from type in types
                 where type.BaseType == typeof(MulticastDelegate) // Enumerbale.Where and anonymous method.
                 group type by type.Namespace into namespaceTypes // Enumerbale.GroupBy and anonymous method.
-                let @namespace = namespaceTypes.Key // Enumerbale.Select and anonymous method.
-                orderby namespaceTypes.Count() descending, @namespace // Enumerbale.OrderByDescending, Enumerbale.ThenBy and anonymous method.
-                select namespaceTypes; // Enumerbale.Select and anonymous method.
+                orderby namespaceTypes.Count() descending, namespaceTypes.Key // Enumerbale.OrderByDescending, Enumerbale.ThenBy and anonymous method.
+                select namespaceTypes; // Omitted.
         }
 
         internal static void ParallelLinq(ParallelQuery<Type> types)
@@ -120,9 +109,8 @@
                 from type in types
                 where type.BaseType == typeof(MulticastDelegate) // ParallelEnumerbale.Where and anonymous method.
                 group type by type.Namespace into namespaceTypes // ParallelEnumerbale.GroupBy and anonymous method.
-                let @namespace = namespaceTypes.Key // ParallelEnumerbale.Select and anonymous method.
-                orderby namespaceTypes.Count() descending, @namespace // ParallelEnumerbale.OrderByDescending, ParallelEnumerbale.ThenBy and anonymous method.
-                select namespaceTypes; // ParallelEnumerbale.Select and anonymous method.
+                orderby namespaceTypes.Count() descending, namespaceTypes.Key // ParallelEnumerbale.OrderByDescending, ParallelEnumerbale.ThenBy and anonymous method.
+                select namespaceTypes; // Omitted.
         }
 
         internal static void LinqToEntities(IQueryable<Product> products)
@@ -131,23 +119,18 @@
                 from product in products
                 where product.ListPrice > 0 // Queryable.Where and expression tree.
                 group product by product.ProductSubcategoryID into subcategoryProducts // Queryable.GroupBy and expression tree.
-                let subcategory = subcategoryProducts.Key // Queryable.Select and expression tree.
-                orderby subcategoryProducts.Count() descending, subcategory // Queryable.OrderByDescending, Queryable.ThenBy and expression tree.
-                select subcategoryProducts; // Queryable.Select and expression tree.
+                orderby subcategoryProducts.Count() descending, subcategoryProducts.Key // Queryable.OrderByDescending, Queryable.ThenBy and expression tree.
+                select subcategoryProducts; // Omitted.
         }
-    }
 
-    internal static partial class QueryExpression
-    {
         internal static void CompiledLinqToObjects(IEnumerable<Type> types)
         {
             IEnumerable<IGrouping<string, Type>> query = types
                 .Where(type => type.BaseType == typeof(MulticastDelegate)) // Enumerbale.Where and anonymous method.
                 .GroupBy(type => type.Namespace) // Enumerbale.GroupBy and anonymous method.
-                .Select(namespaceTypes => new { namespaceTypes, @namespace = namespaceTypes.Key }) // Enumerbale.Select and anonymous method.
-                .OrderByDescending(namespaceTypes => namespaceTypes.namespaceTypes.Count()) // Enumerbale.OrderByDescending and anonymous method.
-                .ThenBy(namespaceTypes => namespaceTypes.@namespace) // Enumerbale.ThenBy and anonymous method.
-                .Select(namespaceTypes => namespaceTypes.namespaceTypes); // Enumerbale.Select and anonymous method.
+                .OrderByDescending(namespaceTypes => namespaceTypes.Count()) // Enumerbale.OrderByDescending and anonymous method.
+                .ThenBy(namespaceTypes => namespaceTypes.Key); // Enumerbale.ThenBy and anonymous method.
+                // .Select(namespaceTypes => namespaceTypes) is omitted.
         }
 
         internal static void CompiledParallelLinq(ParallelQuery<Type> types)
@@ -155,10 +138,9 @@
             ParallelQuery<IGrouping<string, Type>> query = types
                 .Where(type => type.BaseType == typeof(MulticastDelegate)) // ParallelEnumerbale.Where and anonymous method.
                 .GroupBy(type => type.Namespace) // ParallelEnumerbale.GroupBy and anonymous method.
-                .Select(namespaceTypes => new { namespaceTypes, @namespace = namespaceTypes.Key }) // ParallelEnumerbale.Select and anonymous method.
-                .OrderByDescending(@t => @t.namespaceTypes.Count()) // ParallelEnumerbale.OrderByDescending and anonymous method.
-                .ThenBy(namespaceTypes => namespaceTypes.@namespace) // ParallelEnumerbale.ThenBy and anonymous method.
-                .Select(namespaceTypes => namespaceTypes.namespaceTypes); // ParallelEnumerbale.Select and anonymous method.
+                .OrderByDescending(namespaceTypes => namespaceTypes.Count()) // ParallelEnumerbale.OrderByDescending and anonymous method.
+                .ThenBy(namespaceTypes => namespaceTypes.Key); // ParallelEnumerbale.ThenBy and anonymous method.
+                // .Select(namespaceTypes => namespaceTypes) is omitted.
         }
 
         internal static void CompiledLinqToEntities(IQueryable<Product> products)
@@ -166,10 +148,9 @@
             IQueryable<IGrouping<int?, Product>> query = products
                 .Where(product => product.ListPrice > 0) // Queryable.Where and expression tree.
                 .GroupBy(product => product.ProductSubcategoryID) // Queryable.GroupBy and expression tree.
-                .Select(subcategoryProducts => new { subcategoryProducts, subcategory = subcategoryProducts.Key }) // Queryable.Select and expression tree.
-                .OrderByDescending(subcategoryProducts => subcategoryProducts.subcategoryProducts.Count()) // Queryable.OrderByDescending and expression tree.
-                .ThenBy(subcategoryProducts => subcategoryProducts.subcategory) // Queryable.ThenBy and expression tree.
-                .Select(subcategoryProducts => subcategoryProducts.subcategoryProducts); // Queryable.Select and expression tree.
+                .OrderByDescending(subcategoryProducts => subcategoryProducts.Count()) // Queryable.OrderByDescending and expression tree.
+                .ThenBy(subcategoryProducts => subcategoryProducts.Key); // Queryable.ThenBy and expression tree.
+                // .Select(subcategoryProducts => subcategoryProducts) is omitted.
         }
 
         internal static void QueryExpressionAndMethod(IEnumerable<Product> products)
