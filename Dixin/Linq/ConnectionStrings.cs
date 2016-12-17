a3d4030c9622bf8421dc1a3df8f1e50e34dc0f0b
Dixin Yan <dixinyan@live.com>
2016-12-16 22:20:54 -0800

    Update examples.

--- Dixin/Linq/ConnectionStrings.cs
+++ Dixin/Linq/ConnectionStrings.cs
@@ -1,5 +1,8 @@
 ﻿namespace Dixin.Linq
 {
+    using System.Configuration;
+    using System.Linq;
+
     internal static partial class ConnectionStrings
     {
         internal const string LocalDb = @"Data Source=(LocalDB)\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30";
@@ -7,10 +10,17 @@
 
     internal static partial class ConnectionStrings
     {
-        internal const string AdventureWorks = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AdventureWorks_Data.mdf;Integrated Security=True;Connect Timeout=30";
+        internal static string AdventureWorks { get; } =
+            ConfigurationManager.ConnectionStrings.Cast<ConnectionStringSettings>().FirstOrDefault()?.ConnectionString
+            ?? @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AdventureWorks_Data.mdf;Integrated Security=True;Connect Timeout=30";
     }
 
 #if DEMO
+    internal static partial class ConnectionStrings
+    {
+        internal const string AdventureWorks = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AdventureWorks_Data.mdf;Integrated Security=True;Connect Timeout=30";
+    }
+
     internal static partial class ConnectionStrings
     {
         static ConnectionStrings()
---
+++ Dixin/Linq/TraceExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Dixin.Linq
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public static class TraceExtensions
+    {
+        public static T TraceLine<T>(this T value, Func<T, string> messageFactory = null)
+        {
+            messageFactory = messageFactory ?? (_ => value?.ToString());
+            System.Diagnostics.Trace.WriteLine(messageFactory(value));
+            return value;
+        }
+
+        public static T Trace<T>(this T value, Func<T, string> messageFactory = null)
+        {
+            messageFactory = messageFactory ?? (_ => value?.ToString());
+            System.Diagnostics.Trace.Write(messageFactory(value));
+            return value;
+        }
+
+        public static IEnumerable<T> ForEachTraceLine<T>(this IEnumerable<T> values, Func<T, string> messageFactory = null)
+        {
+            values.ForEach(value => value.TraceLine(messageFactory));
+            return values;
+        }
+
+        public static IEnumerable<T> ForEachTrace<T>(this IEnumerable<T> values, Func<T, string> messageFactory = null)
+        {
+            values.ForEach(value => value.Trace(messageFactory));
+            return values;
+        }
+    }
+}
