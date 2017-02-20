96b2a8fce2592a8c2f2507929c2da237805f7b33
Dixin Yan <dixinyan@live.com>
2017-02-20 08:48:29 -0800

    Update examples of document build.

--- Dixin.Office.HtmlToWord/Program.cs
+++ Dixin.Office.HtmlToWord/Program.cs
@@ -7,6 +7,7 @@
     using System.Linq;
     using System.Net;
     using System.Text;
+    using System.Threading.Tasks;
 
     using CsQuery;
     using CsQuery.ExtensionMethods;
@@ -15,6 +16,8 @@
 
     using Microsoft.FSharp.Linq.RuntimeHelpers;
     using Microsoft.Office.Interop.Word;
+
+    using Task = System.Threading.Tasks.Task;
 
     using static System.FormattableString;
 
@@ -35,80 +38,83 @@
     {
         private static void Main(string[] arguments)
         {
-            string outputDirectory = arguments.Any() && !string.IsNullOrWhiteSpace(arguments.First())
-                ? arguments.First()
-                : (PathHelper.TryGetOneDriveRoot(out outputDirectory)
-                    ? Path.Combine(outputDirectory, @"Share\Book")
-                    : Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
-            Trace.WriteLine(Invariant($"Output directory {outputDirectory}."));
-            Html html = DownloadHtml();
-            SaveDocument(html, Path.Combine(outputDirectory, Invariant($"{html.Title}.doc")));
+            BuildDocumentsAsync(arguments.FirstOrDefault()).Wait();
         }
 
-        private static Html DownloadHtml(
-            string indexUrl = @"http://weblogs.asp.net/dixin/linq-via-csharp", int downloadThreadPerProcessor = 10)
+        private static async Task BuildDocumentsAsync(string outputDirectory, string oneDriveDirectory = @"Share\Book")
+        {
+            if (string.IsNullOrWhiteSpace(outputDirectory))
+            {
+                outputDirectory = PathHelper.TryGetOneDriveRoot(out outputDirectory)
+                    ? Path.Combine(outputDirectory, oneDriveDirectory)
+                    : Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            }
+
+            Trace.WriteLine(Invariant($"Output directory {outputDirectory}."));
+            Html html = await DownloadHtmlAsync();
+            await SaveDocumentsAsync(html, Path.Combine(outputDirectory, Invariant($"{html.Title}.doc")), Path.Combine(outputDirectory, Invariant($"{html.Title}.pdf")));
+        }
+
+        private static async Task<Html> DownloadHtmlAsync(
+            string indexUrl = @"http://weblogs.asp.net/dixin/linq-via-csharp")
         {
             using (WebClient indexClient = new WebClient())
             {
                 indexClient.Encoding = Encoding.UTF8;
                 Trace.WriteLine(Invariant($"Downloading {indexUrl}."));
-                CQ indexPage = indexClient.DownloadString(indexUrl);
+                CQ indexPage = await indexClient.DownloadStringTaskAsync(indexUrl);
 
                 CQ article = indexPage["article.blog-post"];
-                IEnumerable<IGrouping<string, Tuple<string, string>>> chapters = article
+                IGrouping<string, Tuple<string, string>>[] chapters = await Task.WhenAll(article
                     .Children("ol")
                     .Children("li")
                     .Select(chapter => chapter.Cq())
                     .AsParallel()
                     .AsOrdered()
-                    .WithDegreeOfParallelism(Environment.ProcessorCount * downloadThreadPerProcessor)
-                    .Select(chapter =>
-                        {
-                            Tuple<string, string>[] sections = chapter.Find("h2")
-                                .Select(section => section.Cq().Find("a:last"))
-                                .AsParallel()
-                                .AsOrdered()
-                                .WithDegreeOfParallelism(Environment.ProcessorCount * downloadThreadPerProcessor)
-                                .Select(section =>
+                    .Select(async chapter =>
+                    {
+                        Tuple<string, string>[] sections = await Task.WhenAll(chapter.Find("h2")
+                            .Select(section => section.Cq().Find("a:last"))
+                            .AsParallel()
+                            .AsOrdered()
+                            .Select(async section =>
+                            {
+                                string sectionUrl = section.Attr<string>("href");
+                                Trace.WriteLine(Invariant($"Downloading {sectionUrl}."));
+                                using (WebClient sectionClient = new WebClient())
                                 {
-                                    string sectionUrl = section.Attr<string>("href");
-                                    Trace.WriteLine(Invariant($"Downloading {sectionUrl}."));
-                                    using (WebClient sectionClient = new WebClient())
-                                    {
-                                        sectionClient.Encoding = Encoding.UTF8;
-                                        CQ sectionPage = sectionClient.DownloadString(sectionUrl);
+                                    sectionClient.Encoding = Encoding.UTF8;
+                                    CQ sectionPage = await sectionClient.DownloadStringTaskAsync(sectionUrl);
 
-                                        CQ sectionArticle = sectionPage["article.blog-post"];
-                                        sectionArticle.Children("header").Remove();
-                                        Enumerable
-                                            .Range(1, 7)
-                                            .Reverse()
-                                            .ForEach(i => sectionArticle
-                                                    .Find(Invariant($"h{i}")).Contents().Unwrap()
-                                                    .Wrap(Invariant($"<h{i + 2}/>"))
-                                                    .Parent()
-                                                    .Find("a").Contents().Unwrap());
-                                        sectionArticle.Find("p")
-                                            .Select(paragraph => paragraph.Cq())
-                                            .ForEach(paragraph =>
-                                                    {
-                                                        string paragraphText = paragraph.Text().Trim();
-                                                        if ((paragraph.Children().Length == 0
-                                                            && string.IsNullOrWhiteSpace(paragraphText))
-                                                            || paragraphText.StartsWith("[LinQ via C#", StringComparison.OrdinalIgnoreCase))
-                                                        {
-                                                            paragraph.Remove();
-                                                        }
-                                                    });
-                                        return Tuple.Create(section.Text().Trim(), sectionArticle.Html());
-                                    }
-                                })
-                                .ToArray();
-                            return new Grouping<string, Tuple<string, string>>(
-                                chapter.Find("h1").Text().Trim(),
-                                sections);
-                        })
-                    .ToArray();
+                                    CQ sectionArticle = sectionPage["article.blog-post"];
+                                    sectionArticle.Children("header").Remove();
+                                    Enumerable
+                                        .Range(1, 7)
+                                        .Reverse()
+                                        .ForEach(i => sectionArticle
+                                            .Find(Invariant($"h{i}")).Contents().Unwrap()
+                                            .Wrap(Invariant($"<h{i + 2}/>"))
+                                            .Parent()
+                                            .Find("a").Contents().Unwrap());
+                                    sectionArticle.Find("p")
+                                        .Select(paragraph => paragraph.Cq())
+                                        .ForEach(paragraph =>
+                                        {
+                                            string paragraphText = paragraph.Text().Trim();
+                                            if ((paragraph.Children().Length == 0
+                                                && string.IsNullOrWhiteSpace(paragraphText))
+                                                || paragraphText.StartsWith("[LinQ via C#", StringComparison.OrdinalIgnoreCase))
+                                            {
+                                                paragraph.Remove();
+                                            }
+                                        });
+                                    return Tuple.Create(section.Text().Trim(), sectionArticle.Html());
+                                }
+                            }));
+                        return new Grouping<string, Tuple<string, string>>(
+                            chapter.Find("h1").Text().Trim(),
+                            sections);
+                    }));
 
                 return new Html(
                     indexPage["title"].Text().Replace("Dixin's Blog -", string.Empty).Trim(),
@@ -116,36 +122,41 @@
             }
         }
 
-        private static void SaveDocument(Html html, string outputDocument)
+        private static async Task SaveDocumentsAsync(Html html, string outputDocument, string exportDocument)
         {
             string tempHtmlFile = Path.ChangeExtension(Path.GetTempFileName(), "htm");
             string htmlContent = html.TransformText();
             Trace.WriteLine(Invariant($"Saving HTML as {tempHtmlFile}, {htmlContent.Length}."));
-            File.WriteAllText(tempHtmlFile, htmlContent);
+            using (StreamWriter writer = new StreamWriter(new FileStream(
+                path: tempHtmlFile, mode: FileMode.Create, access: FileAccess.Write,
+                share: FileShare.Read, bufferSize: 4096, useAsync: true)))
+            {
+                await writer.WriteAsync(htmlContent);
+            }
 
             string template = Path.Combine(PathHelper.ExecutingDirectory(), "Book.dot");
             ConvertDocument(
                 tempHtmlFile, WdOpenFormat.wdOpenFormatWebPages,
                 outputDocument, WdSaveFormat.wdFormatDocument,
+                exportDocument, WdExportFormat.wdExportFormatPDF,
                 document => FormatDocument(document, html, template));
         }
 
-        private static void FormatDocument(Document document, Html html, string template, string author = "Dixin Yan", int downloadThreadPerProcessor = 10)
+        private static void FormatDocument(Document document, Html html, string template, string author = "Dixin Yan")
         {
             document.InlineShapes
-                    .Cast<InlineShape>()
-                    .Where(shape => shape.Type == WdInlineShapeType.wdInlineShapeLinkedPicture)
-                    .AsParallel()
-                    .WithDegreeOfParallelism(Environment.ProcessorCount * downloadThreadPerProcessor)
-                    .ForAll(picture =>
-                        {
-                            Trace.WriteLine(Invariant($"Downloading {picture.LinkFormat.SourceFullName}"));
-                            picture.LinkFormat.SavePictureWithDocument = true;
-                            if (picture.Width > 470)
-                            {
-                                picture.Width = 470;
-                            }
-                        });
+                .Cast<InlineShape>()
+                .Where(shape => shape.Type == WdInlineShapeType.wdInlineShapeLinkedPicture)
+                .AsParallel()
+                .ForAll(picture =>
+                {
+                    Trace.WriteLine(Invariant($"Downloading {picture.LinkFormat.SourceFullName}"));
+                    picture.LinkFormat.SavePictureWithDocument = true;
+                    if (picture.Width > 470)
+                    {
+                        picture.Width = 470;
+                    }
+                });
 
             Trace.WriteLine(Invariant($"Applying template {template}"));
             document.set_AttachedTemplate(template);
@@ -172,18 +183,19 @@
             range.InsertBreak(WdBreakType.wdPageBreak);
 
             document.Sections.Cast<Section>().ForEach(section =>
-                {
-                    range = section.Headers[WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
-                    range.Fields.Add(range, WdFieldType.wdFieldStyleRef, @"""Heading 1""", true);
+            {
+                range = section.Headers[WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                range.Fields.Add(range, WdFieldType.wdFieldStyleRef, @"""Heading 1""", true);
 
-                    section.Footers[WdHeaderFooterIndex.wdHeaderFooterPrimary].PageNumbers.Add(
-                        WdPageNumberAlignment.wdAlignPageNumberCenter);
-                });
+                section.Footers[WdHeaderFooterIndex.wdHeaderFooterPrimary].PageNumbers.Add(
+                    WdPageNumberAlignment.wdAlignPageNumberCenter);
+            });
         }
 
         private static void ConvertDocument(
-            string inputFile, WdOpenFormat inputFormat,
-            string outputFile, WdSaveFormat outputFormat,
+            string openFile, WdOpenFormat openFormat,
+            string saveFile, WdSaveFormat saveFormat,
+            string exportFile, WdExportFormat exportFormat,
             Action<Document> format = null,
             bool isWordVisible = false)
         {
@@ -192,14 +204,16 @@
             {
                 word = new Application { Visible = isWordVisible };
 
-                Trace.WriteLine(Invariant($"Opening {inputFile} as {inputFormat}."));
-                word.Documents.Open(inputFile, Format: inputFormat);
-                Document document = word.Documents[inputFile];
+                Trace.WriteLine(Invariant($"Opening {openFile} as {openFormat}."));
+                word.Documents.Open(openFile, Format: openFormat);
+                Document document = word.Documents[openFile];
 
                 format?.Invoke(document);
 
-                Trace.WriteLine(Invariant($"Saving {outputFile} as {outputFormat}"));
-                document.SaveAs2(outputFile, outputFormat);
+                Trace.WriteLine(Invariant($"Saving {saveFile} as {saveFormat}"));
+                document.SaveAs2(saveFile, saveFormat);
+                Trace.WriteLine(Invariant($"Exporting {exportFile} as {exportFormat}"));
+                //document.ExportAsFixedFormat(exportFile, exportFormat, CreateBookmarks: WdExportCreateBookmarks.wdExportCreateHeadingBookmarks);
             }
             finally
             {
