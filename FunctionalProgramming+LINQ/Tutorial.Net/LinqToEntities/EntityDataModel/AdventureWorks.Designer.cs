1bb25828a9d935034d8a8e0c96fd1073f09e1e6a
Dixin Yan <dixinyan@live.com>
2018-08-15 00:27:42 -0700

    Update examples.

--- FunctionalProgramming+LINQ/Tutorial.Net/LinqToEntities/EntityDataModel/AdventureWorks.Designer.cs
+++ FunctionalProgramming+LINQ/Tutorial.Net/LinqToEntities/EntityDataModel/AdventureWorks.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Data\GitHub\CodeSnippets\Tutorial.Net\LinqToEntities\EntityDataModel\AdventureWorks.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\User\GitHub\CodeSnippets\FunctionalProgramming+LINQ\Tutorial.Net\LinqToEntities\EntityDataModel\AdventureWorks.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
