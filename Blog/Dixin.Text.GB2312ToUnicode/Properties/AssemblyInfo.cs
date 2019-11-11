6a5be03d0da9b616f96238d8808c5f79fa8c7f52
Dixin <dixinyan@live.com>
2019-11-11 03:31:25 -0800

    Upgrade to .NET Core 3.0 and .NET Framework 4.8.

--- Blog/Dixin.Text.GB2312ToUnicode/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Dixin.Text.GB2312ToUnicode")]
-
-[assembly: Guid("8d4cd5e6-3a6a-402a-a286-d9f79d11defd")]
---
+++ Blog/Dixin.Text.GB2312ToUtf8/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Dixin.Text.GB2312ToUnicode
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            string path = args.First();
+            if (Directory.Exists(path))
+            {
+                await Task.WhenAll(Directory.EnumerateFiles(path, args.Length >= 2 ? args[1] : "*.txt", SearchOption.AllDirectories)
+                    .ToList()
+                    .Select(async file => await ConvertAsync(Encoding.GetEncoding("gb2312"), Encoding.UTF8, file)));
+            }
+            else if (File.Exists(path))
+            {
+                await ConvertAsync(Encoding.GetEncoding("gb2312"), Encoding.UTF8, path);
+            }
+            else
+            {
+                throw new ArgumentException("Provided path does not exist.", nameof(args));
+            }
+        }
+
+        public static async Task ConvertAsync(Encoding from, Encoding to, string fromPath, string toPath = null)
+        {
+            byte[] fromBytes = await File.ReadAllBytesAsync(fromPath);
+            byte[] toBytes = Encoding.Convert(from, to, fromBytes);
+            await File.WriteAllBytesAsync(toPath ?? fromPath, toBytes);
+        }
+    }
+}
