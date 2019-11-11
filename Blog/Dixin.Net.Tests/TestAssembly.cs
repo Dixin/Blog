aa5c950aa4fcd49db50cf3d0fe1d59b324853780
Dixin <dixinyan@live.com>
2019-11-11 03:11:36 -0800

    Fix unit tests.

---
+++ Blog/Dixin.Net.Tests/TestAssembly.cs
@@ -0,0 +1,20 @@
+﻿namespace Dixin.Tests
+{
+    using System;
+    using System.IO;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public static class TestAssembly
+    {
+        [AssemblyInitialize]
+        public static void DataDirectory
+            (TestContext testContext)
+        {
+            AppDomain.CurrentDomain.SetData(
+                nameof(DataDirectory),
+                new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Data")).FullName);
+        }
+    }
+}
