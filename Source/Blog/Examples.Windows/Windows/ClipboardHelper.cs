0046baf7d3a05157b5be1b8393fd4cf06ad51ce6
Dixin <dixinyan@live.com>
2020-11-15 15:57:12 -0800

    Cleanup reference.

--- Source/Blog/Examples.Windows/Windows/ClipboardHelper.cs
+++ Source/Blog/Examples.Windows/Windows/ClipboardHelper.cs
@@ -9,16 +9,13 @@
         public static string GetText
             (TextDataFormat format = TextDataFormat.UnicodeText) => ThreadHelper.Sta(() => Clipboard.GetText(format));
 
-        public static void SetText
-            (string text, TextDataFormat format = TextDataFormat.UnicodeText) =>
+        public static void SetText(string text, TextDataFormat format = TextDataFormat.UnicodeText) =>
                 ThreadHelper.Sta(() => Clipboard.SetText(text, format));
 
         [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        public static IDataObject GetData
-            () => ThreadHelper.Sta(Clipboard.GetDataObject);
+        public static IDataObject GetData() => ThreadHelper.Sta(Clipboard.GetDataObject);
 
-        public static void SetDataObject
-            (object data, bool copy = false) =>
-                ThreadHelper.Sta(() => Clipboard.SetDataObject(data, copy));
+        public static void SetDataObject(object data, bool copy = false) =>
+            ThreadHelper.Sta(() => Clipboard.SetDataObject(data, copy));
     }
 }
