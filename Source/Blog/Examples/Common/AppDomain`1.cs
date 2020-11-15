68b53ce71a9d228ae18e47ab30fbf51e0d2aa12a
Dixin <dixinyan@live.com>
2020-11-14 18:53:07 -0800

    Move Windows code.

--- Source/Blog/Examples/Common/AppDomain`1.cs
+++ Source/Blog/Examples/Common/AppDomain`1.cs
@@ -5,27 +5,34 @@
     internal sealed class AppDomain<TMarshalByRefObject> : IDisposable
         where TMarshalByRefObject : MarshalByRefObject
     {
-        private AppDomain appDomain;
+        private AppDomain? appDomain;
 
         internal AppDomain(params object[] args)
         {
             Type type = typeof(TMarshalByRefObject);
             this.appDomain = AppDomain.CreateDomain($"{nameof(AppDomain<TMarshalByRefObject>)} {Guid.NewGuid()}");
-            this.MarshalByRefObject = (TMarshalByRefObject)this.appDomain.CreateInstanceAndUnwrap(
-                type.Assembly.FullName, type.FullName, false, default, null, args, null, null);
+            this.MarshalByRefObject = (TMarshalByRefObject?)this.appDomain.CreateInstanceAndUnwrap(
+                type.Assembly.FullName ?? throw new ArgumentException(nameof(TMarshalByRefObject)), 
+                type.FullName ?? throw new ArgumentException(nameof(TMarshalByRefObject)), 
+                false, 
+                default, 
+                null, 
+                args, 
+                null, 
+                null);
         }
 
-        internal TMarshalByRefObject MarshalByRefObject { get; private set; }
+        internal TMarshalByRefObject? MarshalByRefObject { get; private set; }
 
         public void Dispose()
         {
             if (this.MarshalByRefObject is IDisposable disposable)
             {
-                disposable.Dispose();
+                disposable?.Dispose();
                 this.MarshalByRefObject = null;
             }
 
-            if (this.appDomain != null)
+            if (this.appDomain is not null)
             {
                 AppDomain.Unload(this.appDomain);
                 this.appDomain = null;
