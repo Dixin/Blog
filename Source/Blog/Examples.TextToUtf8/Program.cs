168abf90ab893de92df34404c2660c1ce0c348b6
Dixin <dixinyan@live.com>
2020-11-16 12:52:37 -0800

    Upgrade to C# 9.

--- Source/Blog/Examples.TextToUtf8/Program.cs
+++ Source/Blog/Examples.TextToUtf8/Program.cs
@@ -1,133 +1,133 @@
-﻿namespace Examples.Text.ToUtf8
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ude;
+
+const int MaxDegreeOfParallelism = 4;
+
+string path = args[0];
+string searchPattern = args.Length >= 2 ? args[1] : "*.txt";
+int degree = args.Length >= 3 && int.TryParse(args[2], out degree) ? degree : Math.Max(Environment.ProcessorCount, MaxDegreeOfParallelism);
+Encoding? fromEncoding = args.Length >= 4 ? Encoding.GetEncoding(args[3]) : null;
+
+if (Directory.Exists(path))
 {
-    using System;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Ude;
+    Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories)
+        .AsParallel()
+        .WithDegreeOfParallelism(degree)
+        .ForAll(file => ConvertAsync(file, fromEncoding).Wait());
+}
+else if (File.Exists(path))
+{
+    await ConvertAsync(path, fromEncoding);
+}
+else
+{
+    throw new ArgumentException($"The provided path {path} does not exist.", nameof(args));
+}
 
-    internal static class Program
+static void Log(string message)
+{
+    Console.WriteLine(message);
+    Trace.WriteLine(message);
+}
+
+static async Task ConvertAsync(string file, Encoding? fromEncoding)
+{
+    if (fromEncoding is not null)
     {
-        private static readonly byte[] Utf8Bom = Encoding.UTF8.GetPreamble();
-
-        private static readonly Action<string> Log = Console.WriteLine;
-
-        private const int MaxDegreeOfParallelism = 4;
-
-        private static async Task Main(string[] args)
+        if (await file.BackUpAndConvertAsync(fromEncoding))
         {
-            string path = args[0];
-            string searchPattern = args.Length >= 2 ? args[1] : "*.txt";
-            int degree = args.Length >= 3 && int.TryParse(args[2], out degree) ? degree : Math.Max(Environment.ProcessorCount, MaxDegreeOfParallelism);
-            Encoding? fromEncoding = args.Length >= 4 ? Encoding.GetEncoding(args[3]) : null;
-            if (Directory.Exists(path))
+            Log($"Converted {fromEncoding}, {file}");
+        }
+    }
+    else
+    {
+        await using FileStream fileStream = File.OpenRead(file);
+        CharsetDetector detector = new CharsetDetector();
+        detector.Feed(fileStream);
+        detector.DataEnd();
+        if (detector.Confidence > 0.5 && !string.IsNullOrWhiteSpace(detector.Charset))
+        {
+            try
             {
-                Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories)
-                    .AsParallel()
-                    .WithDegreeOfParallelism(degree)
-                    .ForAll(file => ConvertAsync(file, fromEncoding).Wait());
+                fromEncoding = Encoding.GetEncoding(detector.Charset);
+
             }
-            else if (File.Exists(path))
+            catch (ArgumentException exception)
             {
-                await ConvertAsync(path, fromEncoding);
+                Log($"!Not supported {detector.Charset}, {detector.Confidence}, {file}. {exception}");
+                return;
             }
-            else
+
+            if (await file.BackUpAndConvertAsync(fromEncoding))
             {
-                throw new ArgumentException("Provided path does not exist.", nameof(args));
+                Log($"Converted {detector.Charset}, {detector.Confidence}, {file}");
             }
         }
-
-        private static async ValueTask<bool> HasUtf8BomAsync(this string file)
+        else
         {
-            await using FileStream stream = File.OpenRead(file);
-            byte[] head = new byte[Utf8Bom.Length];
-            int read = await stream.ReadAsync(head, 0, head.Length);
-            return read == Utf8Bom.Length && Utf8Bom.SequenceEqual(head);
+            Log($"!Not detected {detector.Charset}, {detector.Confidence}, {file}");
+        }
+    }
+}
+
+internal static class Extensions
+{
+    private static readonly byte[] Utf8Bom = Encoding.UTF8.GetPreamble();
+
+    private static async ValueTask<bool> HasUtf8BomAsync(this string file)
+    {
+        await using FileStream stream = File.OpenRead(file);
+        byte[] head = new byte[Utf8Bom.Length];
+        int read = await stream.ReadAsync(head, 0, head.Length);
+        return read == Utf8Bom.Length && Utf8Bom.SequenceEqual(head);
+    }
+
+    private static void RemoveReadOnly(this string file)
+    {
+        FileInfo fileInfo = new FileInfo(file);
+        if (fileInfo.IsReadOnly)
+        {
+            fileInfo.IsReadOnly = false;
+        }
+    }
+
+    private static void BackUp(this string file)
+    {
+        string backUpFile = file + ".bak";
+        if (File.Exists(backUpFile))
+        {
+            backUpFile.RemoveReadOnly();
         }
 
-        private static async Task ConvertAsync(string file, Encoding? fromEncoding)
+        File.Copy(file, backUpFile, true);
+    }
+
+    internal static async Task<bool> BackUpAndConvertAsync(this string file, Encoding fromEncoding)
+    {
+        if (Encoding.UTF8.Equals(fromEncoding) && await file.HasUtf8BomAsync())
         {
-            if (fromEncoding is not null)
-            {
-                if (await BackUpAndConvertAsync(file, fromEncoding))
-                {
-                    Log($"Converted {fromEncoding}, {file}");
-                }
-            }
-            else
-            {
-                await using FileStream fileStream = File.OpenRead(file);
-                CharsetDetector detector = new CharsetDetector();
-                detector.Feed(fileStream);
-                detector.DataEnd();
-                if (detector.Confidence > 0.5 && !string.IsNullOrWhiteSpace(detector.Charset))
-                {
-                    try
-                    {
-                        fromEncoding = Encoding.GetEncoding(detector.Charset);
-
-                    }
-                    catch (ArgumentException exception)
-                    {
-                        Log($"!Not supported {detector.Charset}, {detector.Confidence}, {file}. {exception}");
-                        return;
-                    }
-
-                    if (await BackUpAndConvertAsync(file, fromEncoding))
-                    {
-                        Log($"Converted {detector.Charset}, {detector.Confidence}, {file}");
-                    }
-                }
-                else
-                {
-                    Log($"!Not detected {detector.Charset}, {detector.Confidence}, {file}");
-                }
-            }
+            return false;
         }
 
-        private static void RemoveReadOnly(this string file)
+        file.RemoveReadOnly();
+        file.BackUp();
+        await file.ConvertAsync(fromEncoding, Encoding.UTF8, bom: Utf8Bom);
+        return true;
+    }
+
+    private static async Task ConvertAsync(this string file, Encoding from, Encoding to, string? toPath = null, byte[]? bom = null)
+    {
+        byte[] fromBytes = await File.ReadAllBytesAsync(file);
+        byte[] toBytes = Encoding.Convert(from, to, fromBytes);
+        if (bom is not null && !bom.SequenceEqual(toBytes.Take(bom.Length)))
         {
-            FileInfo fileInfo = new FileInfo(file);
-            if (fileInfo.IsReadOnly)
-            {
-                fileInfo.IsReadOnly = false;
-            }
+            toBytes = bom.Concat(toBytes).ToArray();
         }
-
-        private static void BackUp(this string file)
-        {
-            string backUpFile = file + ".bak";
-            if (File.Exists(backUpFile))
-            {
-                backUpFile.RemoveReadOnly();
-            }
-
-            File.Copy(file, backUpFile, true);
-        }
-
-        private static async Task<bool> BackUpAndConvertAsync(string file, Encoding fromEncoding)
-        {
-            if (Encoding.UTF8.Equals(fromEncoding) && await file.HasUtf8BomAsync())
-            {
-                return false;
-            }
-
-            file.RemoveReadOnly();
-            file.BackUp();
-            await ConvertAsync(fromEncoding, Encoding.UTF8, file, bom: Utf8Bom);
-            return true;
-        }
-
-        private static async Task ConvertAsync(Encoding from, Encoding to, string file, string? toPath = null, byte[]? bom = null)
-        {
-            byte[] fromBytes = await File.ReadAllBytesAsync(file);
-            byte[] toBytes = Encoding.Convert(from, to, fromBytes);
-            if (bom is not null && !bom.SequenceEqual(toBytes.Take(bom.Length)))
-            {
-                toBytes = bom.Concat(toBytes).ToArray();
-            }
-            await File.WriteAllBytesAsync(string.IsNullOrWhiteSpace(toPath) ? file : toPath, toBytes);
-        }
+        await File.WriteAllBytesAsync(string.IsNullOrWhiteSpace(toPath) ? file : toPath, toBytes);
     }
 }
