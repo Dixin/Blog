34876f06976121f4f04d09c5f0f7f3c6222027ea
Dixin <dixinyan@live.com>
2021-11-11 12:13:29 -0800

    Upgrade to C# 10.

---
+++ Source/Blog/Tools/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System.Collections.Concurrent;
+global using System.Diagnostics;
+global using System.Globalization;
+global using System.Net;
+global using System.Text;
+global using System.Text.RegularExpressions;
+global using System.Xml.Linq;
