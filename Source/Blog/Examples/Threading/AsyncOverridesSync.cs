8274c2b64f4c4d489f089ef495d06d3d5f0a5887
Dixin <dixinyan@live.com>
2026-01-30 00:27:18 -0800

    Update examples.

---
+++ Source/Blog/Examples/Threading/AsyncOverridesSync.cs
@@ -0,0 +1,18 @@
+﻿namespace Examples.Threading;
+
+//Base instance = new Derived();
+//instance.Method();
+
+file class Base
+{
+    public virtual void Method() { }
+}
+
+file class Derived : Base
+{
+    public override async void Method()
+    {
+        base.Method();
+        await Task.Yield();
+    }
+}
